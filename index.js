b2babc7765ce9c8c418db7ffdf73c9707ba6e98d
irriz-jayy <jayfelix789@gmail.com>
2022-12-11 15:33:14 +0300

    Added time event functions but still lacks the 24hours

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ let createEmployeeRecords = function (employeeArrayData) {
   });
 };
 
+//Creating time events
+let createTimeInEvent = function (employee, dateStamp) {
+  let [date, hour] = dateStamp.split("");
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return employee;
+};
+
+let createTimeOutEvent = function (employee, dateStamp) {
+  let [date, hour] = dateStamp.split("");
+  employee.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+};
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
